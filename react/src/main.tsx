960e3ed3436d9aa8edcc47b163005af485896fb9
Max Monis <mmonis77@gmail.com>
2024-07-30 22:37:49 -0400

    Add interop logic and internal-link

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { defineElements } from './interop/defineElements'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <h1>React in AngularJS</h1>
-  </React.StrictMode>,
-)
+// Create custom HTML elements during build
+if (import.meta.env.PROD) defineElements()
+// Otherwise render the WIP component in dev mode
+else
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <div>
+        Replace this div with the component you're currently working on. This
+        will enable you to take advantage of hot reloading. Do not commit
+        changes to this file. When you're ready to try your component out within
+        the Angular app, add it to `interop/elements.ts`.
+      </div>
+    </React.StrictMode>,
+  )
